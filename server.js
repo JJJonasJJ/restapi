bd4bf409572dd8f57e771cdbea85d00629a4f284
Jonas Friberg <jonas.friberg@skola.taby.se>
2025-05-15 12:53:18 +0200

    borde nog commita nästan jklar

--- server.js
+++ server.js
@@ -19,98 +19,114 @@ async function getDBConnnection() {
 }
 
 app.get("/", (req, res) => {
-    res.send(`<h1>EXEMPEL</h1>
-    <ul><li> GET /users</li></ul>`)
+    res.send(`
+      
+      <h1> Dokumentation </h1>
+      <br>
+      <p> GET /users | Ger alla användare i databasen.</p>
+      <p> GET /users/:id | Ger den specifika användaren som specifieras med id-siffran. </p>
+      <p> POST /users | Skapar en ny användare i databasen baserad på information i <br>
+      JSON-format som skickats in. Lösenordet blir hashat samt får salt för säker lagring. </p>
+      <p> PUT /users | Uppdaterar all information om en användare med specifikt id <br> 
+      för att sedan returnera de nya username och name tillbaka till klienten.</p>
+      <p> POST /login | </p>
+ 
+
+      
+      `)
 })
 
+app.get("/users", async function (req, res) {
+
+  let connection = await getDBConnnection()
+  let sql = `SELECT * FROM users`   
+  let [results] = await connection.execute(sql)
+  res.json(results)
+
+});
+
+app.get('/users/:id', async function(req, res) {
+
+  let connection = await getDBConnnection()
+  let sql = "SELECT * FROM users WHERE id = ?"
+  let [results] = await connection.execute(sql, [req.params.id])
+  res.json(results[0]) 
+
+});
+
+
 app.post('/users', async function(req, res) {
-  console.log(req.body)
  
   let connection = await getDBConnnection()
   let sql1 = `INSERT INTO users (username, name, password, salt)
   VALUES (?, ?, ?, ?)`
-  console.log("test");
 
-  const salt = await bcrypt.genSalt(10);  // genererar ett salt till hashning
-  const hashedPassword = await bcrypt.hash(req.body.password, salt); //hashar lösenordet
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt); 
 
-
-  //gör det i del2
-  
-
-  if (req.body && req.body.username) {
+  if (req.body && req.body.username && req.body.name && req.body.password && salt) {
     let [results] = await connection.execute(sql1, [
       req.body.username,
       req.body.name,
       hashedPassword,
-      salt,
+      salt
     ])
-    console.log(results)
+    
   res.json(results)
   }
   
   else {
-    //returnera med felkod 422 Unprocessable entity.
-    //eller 400 Bad Request.
+ 
     res.sendStatus(422)
   }
 });
 
 app.put("/users/:id", async function (req, res) {
-  //kod här för att hantera anrop…
-  let sql = `UPDATE users
-    SET name = ?, password = ?,
-    WHERE id = ?`
+
+  let connection = await getDBConnnection()
+  let sql = `
+    UPDATE users
+    SET username = ?, name = ?, password = ?, salt = ?
+    WHERE id = ?
+    `
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt); 
 
   let [results] = await connection.execute(sql, [
-    req.body.first_name,
-    req.body.last_name,
-    req.params.id,
+    req.body.username,
+    req.body.name,
+    hashedPassword,
+    salt,
+    req.params.id
   ])
-  //kod här för att returnera data
+  
+  let sql2 = "SELECT username, name FROM users WHERE id = ?"
+  let [result] = await connection.execute(sql2, [req.params.id])
+  res.json(result[0])
 })
 
-app.get("/login", function(req,res){
+app.post("/login", async function(req,res){
 
-  let payload = {
-    sub: user.id,         // sub är obligatorisk
-    username: user.name,
-  
+  let connection = await getDBConnnection()
+  let sql = "SELECT * FROM users WHERE username = ?"
+  let [results] = await connection.execute(sql, [req.body.username])
 
-    // kan innehålla ytterligare attribut, t.ex. roller
+ 
+
+
+  const isPasswordValid = await bcrypt.compare(req.body.password, results.salt);
+
+  if (isPasswordValid) {
+    res.status(200)
+    res.send("<p>Hallå, ditt riktiga namn är </p>", results.name)
+  } 
+  else {
+    res.status(400).json({ error: "Wrong username or password!" });
   }
-  let token = jwt.sign(payload, 'generic payload');
+
 
 })
 
-app.get("/auth-test", function (req, res) {
-  let authHeader = req.headers["authorization"] //Hämtar värdet (en sträng med token) från authorization headern i requesten
-  
-  if (authHeader === undefined) {
-    res.status(401).send("Auth token missing.")
-  }
-  
-  let token = authHeader.slice(7) // Tar bort "BEARER " som står i början på strängen.
-  console.log("token: ", token)
-
-  let decoded
-  try {
-    // Verifiera att detta är en korrekt token. Den ska vara:
-    // * skapad med samma secret
-    // * omodifierad
-    // * fortfarande giltig
-    decoded = jwt.verify(token, THESECRET)
-  } catch (err) {
-    // Om något är fel med token så kastas ett error.
-
-    console.error(err) //Logga felet, för felsökning på servern.
-
-    res.status(401).send("Invalid auth token")
-  }
-
-  res.send(decoded) // Skickar tillbaka den avkodade, giltiga, tokenen.
-})
-  
 const port = 3000
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`)
